c347184adab188446579fc7daa9631b515b84f0e
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-12-09 14:58:36 -0500

    Initial creation of Spring Boot Backend

---
+++ P1Demo/src/main/java/com/revature/P1Demo/P1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.P1Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1DemoApplication.class, args);
+	}
+
+}
